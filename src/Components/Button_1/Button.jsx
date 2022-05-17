4eadf8e322c5a4fb130cd457a8cd31a1e8db3fd3
Alex <Alex031994@gmail.com>
2022-05-17 22:40:46 +0300

    added first button

---
+++ src/Components/Button_1/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Button.module.css'
+
+const Button = () => {
+    return (
+        <a className={styles.button}>
+                <span>
+                    text
+                </span>
+                <i></i>
+        </a>
+    );
+};
+
+export default Button;
---
+++ src/Layout/MainContent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './MainContent.module.css'
+import Button from "../Components/Button_1/Button";
+
+const MainContent = () => {
+    return (
+        <div className={styles.main_content}>
+            <Button/>
+        </div>
+    );
+};
+
+export default MainContent;
